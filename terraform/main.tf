9aef20513639cd19ff802a71f63acf8f8076df13
BrianMafuDev <Brian.Mafu@projectolympus.co.za>
2024-07-06 10:16:36 +0200

    Updated terraform removed specific tag name for ecr

--- terraform/main.tf
+++ terraform/main.tf
@@ -48,40 +48,12 @@ resource "aws_iam_role" "ecs_execution_role" {
     Statement = [{
       Effect    = "Allow",
       Principal = { Service = "ecs-tasks.amazonaws.com" },
-      Action    = "sts:AssumeRole"
+      Action    = ["sts:AssumeRole", "logs:CreateLogStream", "logs:PutLogEvents"]
     },
-      {
-        Effect   = "Allow",
-        Action   = [
-          "logs:CreateLogStream",
-          "logs:PutLogEvents"
-        ],
-        Resource = "arn:aws:logs:us-east-1:475408842073:log-group:/ecs/medicaldepartureblogsystem:*"
-      }]
-  })
-}
-resource "aws_iam_policy" "ecs_execution_policy" {
-  name        = "ecs_execution_policy"
-  description = "Policy for ECS execution role"
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect   = "Allow",
-        Action   = [
-          "logs:CreateLogStream",
-          "logs:PutLogEvents"
-        ],
-        Resource = "arn:aws:logs:us-east-1:475408842073:log-group:/ecs/medicaldepartureblogsystem:*"
-      }
     ]
   })
 }
-resource "aws_iam_role_policy_attachment" "ecs_execution_role_policy_attachment" {
-  role       = aws_iam_role.ecs_execution_role.name
-  policy_arn = aws_iam_policy.ecs_execution_policy.arn
-}
+
 resource "aws_iam_role_policy_attachment" "ecs_execution_role_policy_ecr" {
   role       = aws_iam_role.ecs_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
