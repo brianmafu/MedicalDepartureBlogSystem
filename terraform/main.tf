ef70ba21ab32570f7727ee674433c9607360945b
BrianMafuDev <Brian.Mafu@projectolympus.co.za>
2024-07-06 09:34:11 +0200

    Updated terraform removed specific tag name for ecr

--- terraform/main.tf
+++ terraform/main.tf
@@ -42,7 +42,7 @@ resource "random_id" "unique" {
 }
 
 resource "aws_iam_role" "ecs_execution_role" {
-  name               = "ecs_execution_role"
+  name = "ecs_execution_role"
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
     Statement = [{
