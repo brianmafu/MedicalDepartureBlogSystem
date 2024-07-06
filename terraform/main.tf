267b71c717138423dbd943c73c4146571416be85
BrianMafuDev <Brian.Mafu@projectolympus.co.za>
2024-07-06 13:30:30 +0200

    Updated terraform privison mysql database

--- terraform/main.tf
+++ terraform/main.tf
@@ -310,7 +310,7 @@ resource "aws_route53_zone" "main" {
 resource "aws_route53_record" "ecs_dns" {
   zone_id = aws_route53_zone.main.zone_id
   name    = "ecs.medicaldeparturebrian.com"
-  type    = "A"
+  type    = "CNAME"
   ttl     = "300"
   records = [aws_lb.ecs_lb.dns_name]  // Use ALB DNS name here
 }
